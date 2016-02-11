96d0cab0963599833d222c6d2d2bc86edf9d9098
yuanf5 <yuanf5@gene.com>
2016-02-10 21:17:25 -0800

    Update cachematrix.R
    Finish homework wk3

--- cachematrix.R
+++ cachematrix.R
@@ -2,9 +2,20 @@
 ## functions do
 
 ## Write a short comment describing this function
+## This function defines 4 functions: set, get, setinv, getinv
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inv) m <<- inv
+    getinv <- function() m
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
 
@@ -12,4 +23,19 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    m <- x$getinv()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinv(m)
+    m
 }
+
+##Test:
+mx <- matrix(c(7,6,8,5), nrow=2)
+z <- makeCacheMatrix(mx)
+cacheSolve(z)
+cacheSolve(z)  ## Getting cached data when run 2nd time
